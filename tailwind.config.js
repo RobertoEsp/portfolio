f507faf3f3e0eaf522a7e1e654abe2400ff807a0
Roberto Esparza <robert_esparza@hotmail.com>
2024-07-19 15:58:36 -0600

    Change to tailwind styles

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./{src,tailwind}/*.html"],
+  theme: {
+    fontFamily: {
+      mono: ['Source Code Pro'],
+      accent: ['Random Grotesque Spacious'],
+    },
+    extend: {
+      colors: {
+        cloudBlue: '#15202b',
+        accent: '#8264b8',
+        light: '#e5e5e5'
+      },
+    },
+  },
+  plugins: [],
+}
